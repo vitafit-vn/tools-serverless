ed1325df901a290ff8db6a5bab6362e8cc32a6a4
Ethan Nguyen <thanhnx.605@gmail.com>
2019-07-23 16:58:22 +0700

    Update sendEmail handler

--- handler.js
+++ handler.js
@@ -1,4 +1,6 @@
-'use strict';
+// 'use strict';
+
+const aws = require('aws-sdk');
 
 const hello = async event => {
   return {
@@ -14,6 +16,44 @@ const hello = async event => {
   };
 };
 
+const sendEmail = async (/* event */) => {
+  const ses = aws.SES({ region: 'us-east-1' });
+
+  const params = {
+    Destination: {
+      ToAddresses: ['thanhnx.vitafit@gmail.com'],
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: 'UTF-8',
+          Data: '<html><body><b style="color: red;">hello world</b></body></html>',
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'Test HLV Online email',
+      },
+    },
+    Source: 'hlv-online@vitafit.vn',
+  };
+
+  try {
+    const data = await ses.sendEmail(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data, null, 2),
+    };
+  } catch (error) {
+    return {
+      statusCode: 422,
+      body: JSON.stringify({ message: 'Error!', error }, null, 2),
+    };
+  }
+};
+
 module.exports = {
   hello,
+  sendEmail,
 };
