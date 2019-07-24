e47ddf9f9c75f875713ddbba9547d6a1c2827ecd
Ethan Nguyen <thanhnx.605@gmail.com>
2019-07-24 11:00:13 +0700

    Finalize gulp tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,14 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import clean from 'gulp-clean';
 
-gulp.task('default', () =>
+gulp.task('clean', () => gulp.src('./dist', { allowEmpty: true, read: false }).pipe(clean({ allowEmpty: true })));
+
+gulp.task('transpile', () =>
   gulp
     .src('./src/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('./dist'))
 );
+
+gulp.task('default', gulp.series('clean', 'transpile'));
