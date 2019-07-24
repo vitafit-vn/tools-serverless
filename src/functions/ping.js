4a323e05d6f80effc53f6cddac77dd19e26a1aaf
Ethan Nguyen <thanhnx.605@gmail.com>
2019-07-24 15:58:43 +0700

    Refactor ping function

--- src/functions/ping.js
+++
@@ -1,9 +0,0 @@
-import { buildResponseData } from '../utils';
-
-export default function ping(event, context) {
-  return buildResponseData({
-    context,
-    event,
-    message: 'Serverless setup successfully!',
-  });
-}
--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,10 @@
-import ping from './functions/ping';
 import sendEmail from './functions/sendEmail';
+
+function ping(event, context) {
+  return {
+    body: JSON.stringify({ context, event, message: 'Serverless setup successfully!' }, null, 2),
+    statusCode: 200,
+  };
+}
 
 export { ping, sendEmail };
