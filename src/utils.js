7513a93f8e799953dc5d7f49f03b074aab4fdf56
Ethan Nguyen <thanhnx.605@gmail.com>
2019-07-24 16:52:31 +0700

    Refine

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,16 @@
 /* eslint-disable import/prefer-default-export */
 
+const RESPONSE_HEADERS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST',
+  'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type',
+  'Content-Type': 'application/json',
+};
+
 export function buildResponseData({ code = 200, message, ...rest }) {
   return {
     body: JSON.stringify({ message, ...rest }, null, 2),
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST',
-      'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type',
-      'Content-Type': 'application/json',
-    },
+    headers: RESPONSE_HEADERS,
     statusCode: code,
   };
 }
